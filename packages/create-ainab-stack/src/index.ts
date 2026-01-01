1e22ab4c15e9d919c25fe0f75dfb5eda7e83c8bb
Drikus Roor <drikusroor@gmail.com>
2026-01-01 22:12:35 +0100

    chore: Update to 0.0.2

--- packages/create-ainab-stack/src/index.ts
+++ packages/create-ainab-stack/src/index.ts
@@ -11,7 +11,7 @@ export async function main() {
   program
     .name("ainab-stack")
     .description("CLI to scaffold Ainab Stack projects")
-    .version("0.0.1");
+    .version("0.0.2");
 
   program
     .argument("[project-name]", "Name of the project")
